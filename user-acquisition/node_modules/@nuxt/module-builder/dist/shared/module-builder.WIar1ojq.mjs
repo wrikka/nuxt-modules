a719d00a31a599b837c6660ef88a3d5b75959eb4
Veerapong <newkubise@gmail.com>
2026-02-24 02:51:48 +0700

    restructure: move user-acquisition to modules/

--- user-acquisition/node_modules/@nuxt/module-builder/dist/shared/module-builder.WIar1ojq.mjs
+++
@@ -1,5 +0,0 @@
-const name = "@nuxt/module-builder";
-const version = "1.0.2";
-const description = "Complete solution for building Nuxt modules";
-
-export { description as d, name as n, version as v };
