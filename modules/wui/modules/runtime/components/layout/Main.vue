a47c42bb6ca88ac53c6fe8a68d091db4fd206bcc
Veerapong <newkubise@gmail.com>
2026-03-04 02:48:14 +0700

    feat: add Notification component and layout components, update package.json and dependencies

---
+++ modules/wui/modules/runtime/components/layout/Main.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+interface Props {
+  class?: string
+}
+
+const _props = defineProps<Props>()
+</script>
+
+<template>
+  <main :class="['flex-1 min-w-0', _props.class]">
+    <slot />
+  </main>
+</template>
