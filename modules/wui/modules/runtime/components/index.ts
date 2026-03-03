0ca8c1db0ca2afaa4de283c464e827a4c94c928d
Veerapong <newkubise@gmail.com>
2026-03-04 02:52:51 +0700

    feat: update components index with all atomic design components

--- modules/wui/modules/runtime/components/index.ts
+++ modules/wui/modules/runtime/components/index.ts
@@ -1,5 +1,213 @@
-// Atoms
+// ============================================
+// ATOMIC DESIGN SYSTEM - WUI COMPONENTS
+// ============================================
+//
+// Atoms: Basic building blocks (buttons, inputs, labels)
+// Molecules: Groups of atoms working together (form fields, cards)
+// Organisms: Complex components composed of molecules (forms, tables, navigation)
+//
+// ============================================
+
+// ============================================
+// ATOMS - Basic building blocks
+// ============================================
+
+// UI Elements
 export { default as WAvatar } from './atoms/Avatar.vue'
+export { default as WBadge } from './atoms/Badge.vue'
+export { default as WButton } from './atoms/Button.vue'
+export { default as WCheckbox } from './atoms/Checkbox.vue'
+export { default as WChip } from './atoms/Chip.vue'
+export { default as WCode } from './atoms/Code.vue'
+export { default as WContainer } from './atoms/Container.vue'
+export { default as WDivider } from './atoms/Divider.vue'
+export { default as WDropzone } from './atoms/Dropzone.vue'
+export { default as WFlex } from './atoms/Flex.vue'
+export { default as WGrid } from './atoms/Grid.vue'
+export { default as WHeading } from './atoms/Heading.vue'
+export { default as WIcon } from './atoms/Icon.vue'
+export { default as WImage } from './atoms/Image.vue'
+export { default as WInput } from './atoms/Input.vue'
+export { default as WKbd } from './atoms/Kbd.vue'
+export { default as WLabel } from './atoms/Label.vue'
+export { default as WLink } from './atoms/Link.vue'
+export { default as WPopover } from './atoms/Popover.vue'
+export { default as WProgress } from './atoms/Progress.vue'
+export { default as WRadio } from './atoms/Radio.vue'
+export { default as WSelect } from './atoms/Select.vue'
+export { default as WSeparator } from './atoms/Separator.vue'
+export { default as WSlider } from './atoms/Slider.vue'
+export { default as WSpinner } from './atoms/Spinner.vue'
+export { default as WStack } from './atoms/Stack.vue'
+export { default as WSwitch } from './atoms/Switch.vue'
+export { default as WText } from './atoms/Text.vue'
+export { default as WTextarea } from './atoms/Textarea.vue'
+export { default as WToast } from './atoms/Toast.vue'
+export { default as WToggle } from './atoms/Toggle.vue'
+export { default as WTooltip } from './atoms/Tooltip.vue'
+
+// Layout Atoms
+export { default as WAside } from './atoms/Aside.vue'
+export { default as WMain } from './atoms/Main.vue'
+export { default as WSection } from './atoms/Section.vue'
+export { default as WSidebarLayout } from './atoms/SidebarLayout.vue'
+
+// Table Atoms
+export { default as WTableCell } from './atoms/TableCell.vue'
+
+// Menu Atoms
+export { default as WMenuSeparator } from './atoms/MenuSeparator.vue'
+
+// Feedback Atoms
+export { default as WEmpty } from './atoms/Empty.vue'
+export { default as WFormError } from './atoms/FormError.vue'
+export { default as WFormSuccess } from './atoms/FormSuccess.vue'
+export { default as WSkeleton } from './atoms/Skeleton.vue'
+
+// ============================================
+// MOLECULES - Groups of atoms
+// ============================================
+
+// Accordion
+export { default as WAccordion } from './molecules/Accordion.vue'
+export { default as WAccordionItem } from './molecules/AccordionItem.vue'
+
+// Alert & Status
+export { default as WAlert } from './molecules/Alert.vue'
+export { default as WAlertDescription } from './molecules/AlertDescription.vue'
+export { default as WAlertTitle } from './molecules/AlertTitle.vue'
+export { default as WError } from './molecules/Error.vue'
+export { default as WInfo } from './molecules/Info.vue'
+export { default as WSuccess } from './molecules/Success.vue'
+export { default as WWarning } from './molecules/Warning.vue'
+
+// Breadcrumb
+export { default as WBreadcrumb } from './molecules/Breadcrumb.vue'
+export { default as WBreadcrumbs } from './molecules/Breadcrumbs.vue'
+
+// Card
+export { default as WCard } from './molecules/Card.vue'
+export { default as WCardContent } from './molecules/CardContent.vue'
+export { default as WCardDescription } from './molecules/CardDescription.vue'
+export { default as WCardFooter } from './molecules/CardFooter.vue'
+export { default as WCardHeader } from './molecules/CardHeader.vue'
+export { default as WCardTitle } from './molecules/CardTitle.vue'
+
+// Collapsible
+export { default as WCollapsible } from './molecules/Collapsible.vue'
+
+// Color Picker
+export { default as WColorPicker } from './molecules/ColorPicker.vue'
+export { default as WColorPalette } from './molecules/ColorPalette.vue'
+
+// Date & Time
+export { default as WDatePicker } from './molecules/DatePicker.vue'
+export { default as WTimePicker } from './molecules/TimePicker.vue'
+
+// Dialog
+export { default as WDialog } from './molecules/Dialog.vue'
+export { default as WDialogContent } from './molecules/DialogContent.vue'
+export { default as WDialogDescription } from './molecules/DialogDescription.vue'
+export { default as WDialogFooter } from './molecules/DialogFooter.vue'
+export { default as WDialogHeader } from './molecules/DialogHeader.vue'
+export { default as WDialogTitle } from './molecules/DialogTitle.vue'
+
+// File Upload
+export { default as WFileUpload } from './molecules/FileUpload.vue'
+
+// Form Molecules
+export { default as WFormField } from './molecules/FormField.vue'
+export { default as WFormGroup } from './molecules/FormGroup.vue'
+export { default as WInputNumber } from './molecules/InputNumber.vue'
+export { default as WInputPhone } from './molecules/InputPhone.vue'
+export { default as WInputEmail } from './molecules/InputEmail.vue'
+export { default as WInputPassword } from './molecules/InputPassword.vue'
+export { default as WInputOtp } from './molecules/InputOtp.vue'
+export { default as WInputSearch } from './molecules/InputSearch.vue'
+export { default as WSearchInput } from './molecules/SearchInput.vue'
+
+// Loading
+export { default as WLoading } from './molecules/Loading.vue'
+
+// Menu
+export { default as WMenu } from './molecules/Menu.vue'
+export { default as WMenuItem } from './molecules/MenuItem.vue'
+
+// Navigation
+export { default as WPagination } from './molecules/Pagination.vue'
+
+// Panel
+export { default as WPanel } from './molecules/Panel.vue'
+
+// Rating
+export { default as WRating } from './molecules/Rating.vue'
+
+// Stepper
+export { default as WStepper } from './molecules/Stepper.vue'
+
+// Tabs
+export { default as WTabs } from './molecules/Tabs.vue'
+export { default as WTabsContent } from './molecules/TabsContent.vue'
+export { default as WTabsList } from './molecules/TabsList.vue'
+export { default as WTabsTrigger } from './molecules/TabsTrigger.vue'
+
+// Table Molecules
+export { default as WDataTable } from './molecules/DataTable.vue'
+export { default as WTableHeader } from './molecules/TableHeader.vue'
+export { default as WTableRow } from './molecules/TableRow.vue'
+
+// List Molecules
+export { default as WListItem } from './molecules/ListItem.vue'
+
+// Timeline Molecules
+export { default as WTimelineItem } from './molecules/TimelineItem.vue'
+
+// Tree Molecules
+export { default as WTreeItem } from './molecules/TreeItem.vue'
+
+// Toggle
+export { default as WToggleGroup } from './molecules/ToggleGroup.vue'
+export { default as WToggleGroupItem } from './molecules/ToggleGroupItem.vue'
+
+// ============================================
+// ORGANISMS - Complex components
+// ============================================
+
+// Carousel
+export { default as WCarousel } from './organisms/Carousel.vue'
+
+// Data Display Organisms
+export { default as WList } from './organisms/List.vue'
+export { default as WTable } from './organisms/Table.vue'
+export { default as WTimeline } from './organisms/Timeline.vue'
+export { default as WTree } from './organisms/Tree.vue'
+
+// Feedback Organisms
+export { default as WNotification } from './organisms/Notification.vue'
+
+// Form Organism
+export { default as WForm } from './organisms/Form.vue'
+
+// Header
+export { default as WHeader } from './organisms/Header.vue'
+
+// Layout Organisms
+export { default as WResizer } from './organisms/Resizer.vue'
+
+// Lightbox
+export { default as WLightbox } from './organisms/Lightbox.vue'
+
+// Modal
+export { default as WModal } from './organisms/Modal.vue'
+
+// Navigation Organisms
+export { default as WCalendar } from './organisms/Calendar.vue'
+export { default as WPaginationNav } from './organisms/Pagination.vue'
+export { default as WRangeSlider } from './organisms/RangeSlider.vue'
+export { default as WSidebar } from './organisms/Sidebar.vue'
+export { default as WTabBar } from './organisms/TabBar.vue'
+export { default as WTopNav } from './organisms/TopNav.vue'
+
 export { default as WBadge } from './atoms/Badge.vue'
 export { default as WButton } from './atoms/Button.vue'
 export { default as WCheckbox } from './atoms/Checkbox.vue'
