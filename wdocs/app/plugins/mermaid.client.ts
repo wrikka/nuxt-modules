f7c25bd22d732b0d0c10538388099a9e45f6015b
Veerapong <newkubise@gmail.com>
2026-03-04 02:43:01 +0700

    refactor: remove old wdocs module

--- wdocs/app/plugins/mermaid.client.ts
+++
@@ -1,8 +0,0 @@
-import mermaid from "mermaid";
-
-export default defineNuxtPlugin((nuxtApp) => {
-	nuxtApp.hook("page:finish", () => {
-		mermaid.initialize({ startOnLoad: false });
-		void mermaid.run({ querySelector: ".mermaid" });
-	});
-});
--- wdocs/nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineNuxtConfig } from 'nuxt/config'
-
-export default defineNuxtConfig({
-	modules: [
-		"./modules/wdocs.ts",
-		"@wrikka/wdesign",
-	],
-	devtools: { enabled: true },
-});
