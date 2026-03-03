d80f76c414383a85b6d50e14d8ca80728a902aaa
Veerapong <newkubise@gmail.com>
2026-03-04 02:46:41 +0700

    refactor: remove old notes module

--- notes/app/composables/useCommandPalette.ts
+++
@@ -1,9 +0,0 @@
-export const useCommandPalette = createGlobalState(() => {
-  const isOpen = ref(false)
-
-  const open = () => { isOpen.value = true }
-  const close = () => { isOpen.value = false }
-  const toggle = () => { isOpen.value = !isOpen.value }
-
-  return { isOpen, open, close, toggle }
-})
