f849c6602b7a7b18a2d1433e77b26eee4c6231eb
Veerapong <newkubise@gmail.com>
2026-03-04 02:43:21 +0700

    refactor: remove old wslide module

--- wslide/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineVitestConfig, } from "../../packages/config/vitest/config";
-
-export default defineVitestConfig({
-	test: {
-		name: "wslide",
-		globals: true,
-		environment: "happy-dom",
-	},
-},);
