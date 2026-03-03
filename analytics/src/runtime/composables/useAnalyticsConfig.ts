b99426aee69e3903c13fc2b4f23bf3f40baf2a22
Veerapong <newkubise@gmail.com>
2026-03-04 02:42:21 +0700

    refactor: remove old analytics module

--- analytics/src/runtime/composables/useAnalyticsConfig.ts
+++
@@ -1,7 +0,0 @@
-import { useRuntimeConfig } from '#imports';
-import type { AnalyticsConfig } from '#analytics/types';
-
-export const useAnalyticsConfig = () => {
-  const config = useRuntimeConfig();
-  return config.public.analytics as AnalyticsConfig;
-};
